adcb380ea60b393b7ef74cdfb4709ad96bbe0b25
eyern <eyern254@gmail.com>
2025-09-10 08:23:08 +0300

    sitemap & media config

--- core/urls.py
+++ core/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
