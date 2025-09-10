1b5a79d95431084adce2624af447f63cb8680f2b
eyern <eyern254@gmail.com>
2025-09-10 08:46:11 +0300

    shop url

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.utils.regex_helper import normalize
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'shop'
 
@@ -14,3 +16,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
