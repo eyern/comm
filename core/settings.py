14332440fd3fda2ce8845a5721a67d4605d0405c
eyern <eyern254@gmail.com>
2025-09-25 17:33:32 +0300

    setting up env variables

--- core/settings.py
+++ core/settings.py
@@ -164,8 +164,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT= 587 
-EMAIL_HOST_USER = '2ofuscommunication@gmail.com'
-EMAIL_HOST_PASSWORD = 'mcwn gpfp ecsx gqss'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 
 # AWS_QUERYSTRING_AUTH = False
 
