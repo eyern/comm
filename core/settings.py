7d173d5fb89802008c6108ba552af4e8da19aa64
eyern <eyern254@gmail.com>
2025-10-08 07:04:09 +0300

    ttl switch & port

--- core/settings.py
+++ core/settings.py
@@ -162,8 +162,8 @@ PASSWORD_RESET_TIMEOUT_DAYS = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST= 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT= 587 
+EMAIL_USE_SSL = True
+EMAIL_PORT= 465 
 EMAIL_HOST_USER = '2ofuscommunication@gmail.com'
 EMAIL_HOST_PASSWORD = 'mcwn gpfp ecsx gqss'
 
